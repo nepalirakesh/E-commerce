6f89d5e3094ff77ded15a81cb8a25a8a08ae031a
nepalirakesh <kinarisako@gmail.com>
2023-02-10 11:11:22 +0545

    [add]crudmessage for flash message alert

--- resources/views/layouts/dashboard/crudmessage.blade.php
+++ resources/views/layouts/dashboard/crudmessage.blade.php
@@ -1,33 +1,32 @@
-@if(Session::has('success'))
-     
-        <div class="alert alert-primary w-50" id ="success" role="alert" >
-            {{Session::get('success')}}
+@if (Session::has('success'))
+    <div class="container mt-2 w-25 text-center">
+        <div class="alert alert-success" role="alert">
+            {{ Session::get('success') }}
 
-          </div>
-     
+        </div>
+    </div>
 @endif
-@if(Session::has('delete'))
-<div class="container mt-2 w-25 text-center">
-    <div class="alert alert-danger" role="alert">
-        {{Session::get('delete')}}
+@if (Session::has('delete'))
+    <div class="container mt-2 w-25 text-center">
+        <div class="alert alert-danger" role="alert">
+            {{ Session::get('delete') }}
 
-      </div>
-  </div>
-  @endif
-  @if(Session::has('update'))
-<div class="container mt-2 w-25 text-center">
-    <div class="alert alert-success" role="alert">
-        {{Session::get('update')}}
+        </div>
+    </div>
+@endif
+@if (Session::has('update'))
+    <div class="container mt-2 w-25 text-center">
+        <div class="alert alert-success" role="alert">
+            {{ Session::get('update') }}
 
-      </div>
-  </div>
-  @endif
-  @if(Session::has('fail'))
-  <div class="container mt-2 w-25 text-center">
-    <div class="alert alert-danger" role="alert">
-        {{Session::get('fail')}}
+        </div>
+    </div>
+@endif
+@if (Session::has('fail'))
+    <div class="container mt-2 w-25 text-center">
+        <div class="alert alert-danger" role="alert">
+            {{ Session::get('fail') }}
 
-      </div>
-  </div>
-  @endif
-
+        </div>
+    </div>
+@endif

6735bdc69c4afba9330e7fb8e594f4cbf9ff49e4
Sunita-Shrestha <sthasunita987@gmail.com>
2023-03-15 16:52:28 +0545

    [fix] css in admin dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -29,7 +29,7 @@
 
 @section('content')
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div class="content-wrapper" style="height:auto;">
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
